cfa3da35d7d5882f8c9d4bcb829837cdcb3db35e
onekingdev <onekingdev@gmail.com>
2020-12-14 15:03:24 -0600

    logout btn

--- src/containers/Settings/index.tsx
+++ src/containers/Settings/index.tsx
@@ -2,26 +2,45 @@ import React, {useState, useEffect, useRef} from 'react';
 // @ts-ignore
 import {connect} from 'react-redux';
 import './styles.css';
+import Button from "../../components/Button";
+import {IUser} from "../Authentication/interfaces";
+import * as ACTIONS from "../Authentication/store/actions";
+// @ts-ignore
+import {useHistory} from 'react-router-dom';
 
-
-function Settings() {
+function Settings(props: any) {
+    const history = useHistory();
 
     return (
         <div className="settingsContainer">
-
+            <Button
+                loading={props.isFetchingAuthentication}
+                width={200}
+                height={44}
+                text="Logout"
+                glow
+                onClick={() => {
+                    props.logout((success: boolean) => {
+                        if (success) {
+                            history.push(`/`);
+                        }
+                    })
+                }}
+            />
         </div>
     );
 }
 
 const mapStateToProps = (state: any) => {
     return {
-
+        isFetchingAuthentication: state.authState.isFetchingAuthentication,
+        messageCode: state.authState.messageCode
     };
 }
 
 const bindActions = (dispatch: any) => {
     return {
-
+        logout: (callback: (success: boolean) => void) => dispatch(ACTIONS.logout(callback))
     };
 };
 
