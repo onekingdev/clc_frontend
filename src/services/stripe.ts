7fffbd1959a45403d84567e8d37af646b1a7f8f8
onekingdev <onekingdev@gmail.com>
2021-05-12 12:10:15 -0500

    adding env vars to firebase

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -2,8 +2,7 @@ require("dotenv").config();
 
 export const getStripeKey = {
     stripe_publishable_key: (env: string) => {
-        if (env === 'production') return String(process.env.REACT_APP_STRIPE_PROD_KEY);
-        return String(process.env.REACT_APP_STRIPE_DEV_KEY);
-
+        if (env === 'development') return String(process.env.REACT_APP_STRIPE_DEV_KEY);
+        return String(process.env.REACT_APP_STRIPE_PROD_KEY);
     },
 }
