b5c5aa3d441ec74b2cc8833f8a8c22eb94cfa157
onekingdev <onekingdev@gmail.com>
2021-07-07 13:32:10 -0500

    Add devtools for redux

--- src/redux/configureStore.ts
+++ src/redux/configureStore.ts
@@ -1,41 +1,40 @@
-import {applyMiddleware, compose, createStore} from 'redux';
-import {persistStore, persistReducer} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { applyMiddleware, compose, createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // @ts-ignore
-import {createLogger} from 'redux-logger';
-import thunk from 'redux-thunk';
-import rootReducer from './rootReducer';
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
+import rootReducer from "./rootReducer";
 // @ts-ignore
-import devTools from 'remote-redux-devtools';
+import devTools from "remote-redux-devtools";
 
 const persistConfig = {
-    key: 'authState',
-    storage: storage,
-    whitelist: ['authState'] // which reducer want to store
+  key: "authState",
+  storage: storage,
+  whitelist: ["authState"], // which reducer want to store
 };
 
 const logger = createLogger();
 
-const promise = () => (next: any) => (action: any) => (
-    typeof action.then === 'function'
-        ? Promise.resolve(action).then(next, (error: any) => {
-            throw error; // To let the caller handle the rejection
-        })
-        : next(action)
-)
+const promise = () => (next: any) => (action: any) =>
+  typeof action.then === "function"
+    ? Promise.resolve(action).then(next, (error: any) => {
+        throw error; // To let the caller handle the rejection
+      })
+    : next(action);
 
 const enhancer = compose(
-    applyMiddleware(thunk, promise, logger),
-    devTools({
-        name: 'Chip Leader AI',
-        realtime: true,
-    }),
+  applyMiddleware(thunk, promise, logger),
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export default function configureStore(onCompletion: any) {
-    let store: any = createStore(persistedReducer, enhancer);
-    let persistor = persistStore(store);
-    return {store, persistor};
+  let store: any = createStore(
+    persistedReducer,
+    enhancer,
+  );
+  let persistor = persistStore(store);
+  return { store, persistor };
 }
