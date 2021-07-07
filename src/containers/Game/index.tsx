4d863c7f27884ba155971497c5d6a965e5bf3370
onekingdev <onekingdev@gmail.com>
2021-07-07 12:33:34 -0500

    issue CLC-350

--- src/containers/Game/index.tsx
+++ src/containers/Game/index.tsx
@@ -153,13 +153,12 @@ function Game(props: any) {
     }
     
     const forward = () => {
-
         if (useStartIndex) {
             setUseStartIndex(false);
             clearInterval(interval);
             setPause(true)
         }
-
+       
         if (questions.array[questionIndex].hands.length-1 === handIndex) {
             stop();
             setFinished(true);
@@ -174,14 +173,14 @@ function Game(props: any) {
             setHandIndex(index);
             setTableAction(questions.array[questionIndex].hands[index].tableAction);
             setDeleteFolds(true);
-            // setPause(true) // !! not sure about this
+            setPause(true)
         }
 
         if(questions.array[questionIndex].hands[handIndex].amount > callMoney)
         {
             setCallMoney(questions.array[questionIndex].hands[handIndex].amount)
         }
-
+       
         
     }
     
@@ -366,28 +365,6 @@ function Game(props: any) {
         )
     }
 
-    const renderSize = (width: number) => {
-        const digits = width.toString();
-
-        if (width < 600) {
-            return `${0}.${parseInt(digits[0])-1}`;
-        } if (width < 1000) {
-            return `${0}.${parseInt(digits[0])-2}`;
-        } else if (width < 1100) {
-            return `${0}.${parseInt(digits[0])+7}`;
-        } else if (width < 1300) {
-            return `${parseInt(digits[0])}.${parseInt(digits[1])-6}`;
-        } else if (width < 2000) {
-            return `${parseInt(digits[0])}.${parseInt(digits[1])-3}${parseInt(digits[2])-3}`;
-        } else if (width < 2700) {
-            return `${parseInt(digits[0])-1}.${parseInt(digits[1])+3}`;
-        } else if (width < 3000) {
-            return `${parseInt(digits[0])}.${parseInt(digits[1])-6}`;
-        }
-
-        return `${parseInt(digits[0])-1}.${parseInt(digits[1])}${parseInt(digits[2])}`;
-    }
-
     const calculateAllAnte = () => {
         let amount = 0;
         questions.array[questionIndex].hands.forEach((hand: any, index: number) => {
@@ -424,7 +401,7 @@ function Game(props: any) {
     return (
         <ScreenTemplate id="screenTemplate" type={pathname.substr(1, pathname.length)} loading={!props.isFetchingGameData || props.questions.length === 0}>
             {questions.array.length === 0 ? null :
-                <div className="gameWrapper" style={{transform: `scale(${renderSize(width-100)})`}}>
+                <div className="gameWrapper">
                     {showTable ? <div>
                         <div className="gamePokerTableContainer">
                             {!showModal && !rerender && questions.array[questionIndex].players.length > 0 ?
--- src/services/apiMiddleware.ts
+++ src/services/apiMiddleware.ts
@@ -1,36 +1,54 @@
-import axios from 'axios';
-import {apiCloudHostUrl, apiLocalhostUrl, apiStagingHostUrl} from '../helpers/constants';
+import axios from "axios";
+import {
+  apiCloudHostUrl,
+  apiLocalhostUrl,
+  apiStagingHostUrl,
+} from "../helpers/constants";
 const staging = false;
 
 const getApi = () => {
-    if (staging) {
-        return apiStagingHostUrl;
-    }
-    return apiCloudHostUrl;
-}
+  if (staging) {
+    return apiStagingHostUrl;
+  }
+  return apiCloudHostUrl;
+};
 
 const api = {
-    get: async (path: string) =>
-        await axios.get(`${getApi()}/${path}`,{headers: {
-                'Content-Type': 'application/json',
-            }})
-            .then((res: any) => res.data)
-            .catch(error => error),
-    post: async (path: string, data: any, headers?: object) =>
-        await axios.post(`${getApi()}/${path}`, data, headers ? headers : {headers: {
-                'Content-Type': 'application/json',
-            }})
-            .then((res: any) => {
-                console.log(res.data)
-                return res.data
-            })
-            .catch(error => error),
-    put: async (path: string, data: any) =>
-        await axios.put(`${getApi()}/${path}`, data, {headers: {
-                'Content-Type': 'application/json',
-            }})
-            .then((res: any) => res.data)
-            .catch(error => error),
-}
+  get: async (path: string) =>
+    await axios
+      .get(`${getApi()}/${path}`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res: any) => res.data)
+      .catch((error) => error),
+  post: async (path: string, data: any, headers?: object) =>
+    await axios
+      .post(
+        `${getApi()}/${path}`,
+        data,
+        headers
+          ? headers
+          : {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+      )
+      .then((res: any) => {
+        return res.data;
+      })
+      .catch((error) => error),
+  put: async (path: string, data: any) =>
+    await axios
+      .put(`${getApi()}/${path}`, data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((res: any) => res.data)
+      .catch((error) => error),
+};
 
 export default api;
