cfbe178881709dfda829fdda0ac3511264a56fe8
onekingdev <onekingdev@gmail.com>
2021-07-19 12:58:34 -0500

    change stripe dev

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 export const getStripeKey = {
     stripe_publishable_key: (env: string) => {
-        if (env === 'production') return String(process.env.REACT_APP_STRIPE_PROD_KEY);
+        //if (env === 'production') return String(process.env.REACT_APP_STRIPE_PROD_KEY);
         return String(process.env.REACT_APP_STRIPE_DEV_KEY);
 
     },
