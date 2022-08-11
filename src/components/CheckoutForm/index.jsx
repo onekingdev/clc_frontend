7e5d08f8fc06f8a7ea5a1b781126a657d414f181
onekingdev <onekingdev@gmail.com>
2022-08-11 12:40:34 +0900

    add space and dot at the end of sentence

--- src/components/CheckoutForm/index.jsx
+++ src/components/CheckoutForm/index.jsx
@@ -329,7 +329,7 @@ export default function CheckoutForm({
                 </div>
                 <div className="confirm-modal">
                     <div className="text-content">
-                        <span>You are switching from</span> <b>{user.payment.subscriptionType} {user.payment.subscriptionInterval}ly</b> <span>to</span> <b>{subscriptionType} {subscriptionInterval}ly</b> plan.
+                        <span>You are switching from</span> <b>{user.payment.subscriptionType} {user.payment.subscriptionInterval}ly</b> <span>to</span> <b>{subscriptionType} {subscriptionInterval}ly</b> plan. 
                         {
                             (
                                 (user.payment.subscriptionType === 'CL AI' && user.payment.subscriptionInterval === 'year' && subscriptionType === 'CL AI' && subscriptionInterval === 'month') || // 5 in google
@@ -340,8 +340,8 @@ export default function CheckoutForm({
                                 (user.payment.subscriptionType === 'CL AI+' && user.payment.subscriptionInterval === 'year' && subscriptionType === 'CL AI' && subscriptionInterval === 'month') || // 11 in google
                                 (user.payment.subscriptionType === 'CL AI+' && user.payment.subscriptionInterval === 'year' && subscriptionType === 'CL AI' && subscriptionInterval === 'year') // 12 in google=
                             ) ? 
-                            <span>This will go into effect at the end of your annual plan period</span> :
-                            <span>Your current subscription will end today and your new plan will start effective immediately</span>
+                            <span>This will go into effect at the end of your annual plan period.</span> :
+                            <span>Your current subscription will end today and your new plan will start effective immediately.</span>
                         }
                     </div>
                     <Button
